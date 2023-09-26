b2b5fb36caa3e7594f45d3e836fc73392506f131
arian <arian@LAPTOP-U3QLCDBC>
2023-09-25 23:57:54 -0500

    Agregar archivos de proyecto.

---
+++ DPA.Store.DOMAIN/Core/Interfaces/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using DPA.Store.DOMAIN.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPA.Store.DOMAIN.Core.Interfaces
+{
+    public interface IUserRepository
+    {
+        public Task<User> Insert(User user);
+        public Task<bool> SignIn(string email, string clave);
+        public Task<bool> Delete(int id);
+    }
+}
